05112228284624b0d2da0fd3a03d4949f64e14e2
alexstu1 <158611991+alexstu1@users.noreply.github.com>
2025-02-22 17:07:02 -0700

    Add files via upload

--- LA1/src/Music/Controller.java
+++ LA1/src/Music/Controller.java
@@ -10,12 +10,10 @@ import java.util.Scanner;
 public class Controller {
 	public static void main(String[] args) {
 		ArrayList<String> albums = loadAlbums();
-		System.out.println("test");
 		for (String s : albums) {
 			ArrayList<String> album = loadFromFile("./albums/"+s);
 			//Construct store from album here
 		}
-		saveFavorite("test2");
 		/*TODO: Call constructor method and create library from created albumList
 
 		 in each arrayList the following values are stored in the following order:
@@ -101,15 +99,86 @@ public class Controller {
 			for (String s : toSave) {
 				fileWriter.append(s+"\n");
 			}
-			System.out.println("test3");
 			fileWriter.append(songName+"\n");
 			fileWriter.close();
 		} catch (IOException e) {
 			System.out.println("Something went wrong while writing file:");
 			System.out.println("./albums/favorites.txt");
 		}
-		
 	}
+	static ArrayList<String> getRatings(){
+		File file = new File("./albums/favorites.txt");
+		ArrayList<String> favorites = new ArrayList<String>();
+		try {
+			Scanner reader = new Scanner(file);
+			while (reader.hasNext()) {
+				favorites.add(reader.nextLine());
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Something went wrong while reading file:");
+			System.out.println("./albums/favorites.txt");
+		}
+		return favorites;
+	}
+	static ArrayList<String> loadPlaylist(String playListName) {
+		//Assuming the playlist will be its own class, the Arraylist can be changed to however it is handled
+		ArrayList<String> playList = new ArrayList<String>();
+		try {
+			File file = new File("./playlists/"+playListName+".txt"); 
+			Scanner reader = new Scanner(file);
+			while (reader.hasNext()) {
+				playList.add(reader.nextLine());
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Something went wrong while reading file:");
+			System.out.println("./playlists/"+playListName+".txt");
+		}
+		return playList;
+	}
+	static void savePlaylist(String playListName, ArrayList<String> thePlayList) {
+		//Assuming the playlist will be its own class, the Arraylist can be changed to however it is handled
+		try {
+			FileWriter fileWriter = new FileWriter("./playlists/"+playListName+".txt"); 
+			for (String s : thePlayList) {
+				fileWriter.append(s+"\n");
+			}
+			fileWriter.close();
+		} catch (IOException e) {
+			System.out.println("Something went wrong while writing file:");
+			System.out.println("./playlists/"+playListName+".txt");
+		}
+	}
+	static ArrayList<String> getPlaylistNames(){
+		File file = new File("./albums/playlistNames.txt");
+		ArrayList<String> playlists = new ArrayList<String>();
+		try {
+			Scanner reader = new Scanner(file);
+			while (reader.hasNext()) {
+				playlists.add(reader.nextLine());
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Something went wrong while reading file:");
+			System.out.println("./albums/playlistNames.txt");
+		}
+		return playlists;
+	}
+	static void createPlaylist(String newPlaylist) {
+		ArrayList<String> toSave =  getPlaylistNames();
+		try {
+			FileWriter fileWriter = new FileWriter("./albums/playlistNames.txt");
+			for (String s : toSave) {
+				fileWriter.append(s+"\n");
+			}
+			fileWriter.append(newPlaylist+"\n");
+			fileWriter.close();
+		} catch (IOException e) {
+			System.out.println("Something went wrong while writing file:");
+			System.out.println("./albums/playlistNames.txt");
+		}
 	
+	}
 }
 
