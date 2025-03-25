668dd8b33ff92d22b0b1087d01fe4814b60436be
alexstu1 <158611991+alexstu1@users.noreply.github.com>
2025-03-24 21:24:26 -0700

    97.3 coverage

--- Model/PlaylistTest.java
+++ Model/PlaylistTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 class PlaylistTest {
-    // Coverage Achieved: 96%
+    // Coverage Achieved: 97.3%
     private Playlist playlist;
     private Song song1;
     private Song song2;
@@ -14,10 +14,9 @@ class PlaylistTest {
     @BeforeEach
     void setUp() {
         playlist = new Playlist("Test");
-        song1 = new Song("Song 1", "Eric", "LA1");
-        song2 = new Song("Song 2", "Alex", "LA1");
+        song1 = new Song("Song 1", "Eric", "LA1","test",2025);
+        song2 = new Song("Song 2", "Alex", "LA1","test 2", 2024);
     }
-
     @Test
     void testConstructor() {
         assertEquals("Test", playlist.getName());
@@ -40,7 +39,7 @@ class PlaylistTest {
     @Test
     void testGetMatches() {
         playlist.addSong(song1);
-        playlist.addSong(new Song("Song 1", "Not Eric", "Not LA1"));
+        playlist.addSong(new Song("Song 1", "Not Eric", "Not LA1","not test1", 2000));
         
         ArrayList<Song> matches = playlist.getMatches("Song 1");
         assertEquals(2, matches.size());
@@ -58,7 +57,7 @@ class PlaylistTest {
 
     @Test
     void testRemoveSongByTitleAndArtist() {
-        Song song1Dupe = new Song("Song 1", "Not Eric", "LA1");
+        Song song1Dupe = new Song("Song 1", "Not Eric", "LA1","test 3", 2023);
         playlist.addSong(song1);
         playlist.addSong(song1Dupe);
         playlist.addSong(song2);
@@ -80,8 +79,80 @@ class PlaylistTest {
         String output = playlist.toString();
         expected = "Playlist: Test\n" +
                           "Track List:\n" +
-                          "   1. Eric - Song 1 | Appears on: LA1\n";
-                        
+                          "   1. Eric - Song 1 | Genre: LA1 | Appears on: test\n";
         assertTrue(output.equals(expected));
     }
+    @Test
+    void testAddSongIntSong() {
+    	 playlist.addSong(0,song1);
+         assertTrue(playlist.hasSong(song1));
+    }
+    @Test
+    void testRemoveSongInt() {
+    	playlist.addSong(0,song1);
+    	playlist.removeSong(0);
+    	 assertFalse(playlist.hasSong(song1));
+    }
+    @Test
+    void testGetSize() {
+    	assertTrue(0==playlist.getSize());
+    	playlist.addSong(song1);
+    	assertTrue(1==playlist.getSize());
+    	playlist.addSong(song2);
+    	assertTrue(2==playlist.getSize());
+    }
+    @Test 
+    void testGetSong() {
+    	 playlist.addSong(0,song1);
+    	 assertEquals(song1,playlist.getSong(0));
+    }
+    @Test
+    void testContains() {
+    	playlist.addSong(song1);
+    	assertTrue(playlist.contains(song1));
+    	assertFalse(playlist.contains(song2));
+    }
+    @Test
+    void testRemoveSongSong() {
+    	playlist.addSong(0,song1);
+    	playlist.removeSong(0);
+    	assertFalse(playlist.contains(song1));
+    }
+    @Test
+    void testRemoveSongString() {
+    	playlist.addSong(0,song1);
+    	playlist.removeSong("Song 1");
+    	assertFalse(playlist.contains(song1));
+    }
+    @Test
+    void testShuffle() {
+    	playlist.addSong(0,song1);
+    	playlist.addSong(1,song2);
+    	boolean shuffled=false;
+    	playlist.shuffle();
+    	for (int i=0;i<10;i++) {
+    		playlist.shuffle();
+    		if (playlist.getSong(0).equals(song2)) {
+    			shuffled=true;
+    			break;
+    		}
+    	}
+    	assertTrue(shuffled);
+    }
+    @Test
+    void testTopTenString() {
+    	Playlist newPlaylist = new Playlist("Most Played");
+    	String expected = "Playlist: Most Played\n"+
+    			"You dont currently have any tracks on this playlist. Add some to get started!";
+    	assertEquals(expected,newPlaylist.toString());
+    	newPlaylist.addSong(song1);
+    	expected = 	"Playlist: Most Played\n"+
+    				"Track List:\n"+
+    				"   1. Eric - Song 1 | Genre: LA1 | Appears on: test\n";
+    	assertEquals(expected,newPlaylist.toString());
+    	
+    	
+
+    }
+    
 }
